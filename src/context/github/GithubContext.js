dec74df711c83660132ce16c109dcc7ced02c622
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-11-27 21:28:05 +0600

    Added user search functionality.

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,24 +1,31 @@
 import { createContext, useState } from "react";
 
 const GithubContext = createContext();
-
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
 export const GithubProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${GITHUB_URL}/users`);
+  const searchUsers = async (text) => {
+    setLoading(true);
+    const params = new URLSearchParams({
+      q: text,
+    });
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
 
-    const data = await response.json();
+    const { items } = await response.json();
 
-    setUsers(data);
+    setUsers(items);
     setLoading(false);
   };
 
+  const clearUsers = () => {
+    setUsers([]);
+  };
+
   return (
-    <GithubContext.Provider value={{ users, loading, fetchUsers }}>
+    <GithubContext.Provider value={{ users, loading, searchUsers, clearUsers }}>
       {children}
     </GithubContext.Provider>
   );
